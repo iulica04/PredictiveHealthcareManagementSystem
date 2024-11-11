6723feef6c48573b6eb9aad7dd672e1a4d4a7b57
emiliaaa25 <galatanuemilia14@gmail.com>
2024-11-11 15:53:53 +0200

    aduagat in mapping si dependency injection medicalcondition

--- PHMS.WebAPI/Application/Utils/MappingProfile.cs
+++ PHMS.WebAPI/Application/Utils/MappingProfile.cs
@@ -1,5 +1,6 @@
 using Application.Commands.Administrator;
 using Application.Commands.Medic;
+using Application.Commands.MedicalConditionCommands;
 using Application.Commands.Patient;
 using Application.DTOs;
 using AutoMapper;
@@ -19,6 +20,9 @@ namespace Application.Utils
             CreateMap<UpdatePatientCommand, Patient>().ReverseMap();
             CreateMap<Admin, AdminDto>().ReverseMap();
             CreateMap<UpdateAdminCommand, Admin>().ReverseMap();
+            CreateMap<MedicalCondition, MedicalConditionDTO>().ReverseMap();
+            CreateMap<CreateMedicalConditionCommand, MedicalCondition>().ReverseMap();
+            CreateMap<UpdateMedicalConditionCommand, MedicalCondition>().ReverseMap();
         }
     }
 }
