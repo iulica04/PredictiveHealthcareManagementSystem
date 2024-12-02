0079be9ac1d51eaa17d775b2beefa94175948c11
emiliaaa25 <galatanuemilia14@gmail.com>
2024-12-03 00:50:53 +0200

    sters api iau imd inapoi

--- PHMS.WebAPI/Application/Queries/MedicQueries/GetAllMedicsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.DTOs;
-using MediatR;
-
-namespace Application.Queries.MedicQueries
-{
-    public class GetAllMedicsQuery : IRequest<List<MedicDto>>
-    {
-    }
-}
--- PHMS.WebAPI/Domain/Entities/Prescription.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entities
-{
-    public class Prescription
-    {
-        public Guid Id { get; set; }
-        public DateTime DateIssued { get; set; }
-        public required List<Medication> Medications { get; set; }
-    }
-}
